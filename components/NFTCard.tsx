307b50657bd623e8e1a8f71e07bc15ff1d65cf18
xo13s <139127803+xo13s@users.noreply.github.com>
2024-09-17 02:34:00 -0700

    Update NFTCard.tsx

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,44 +1,128 @@
-import {
-  ThirdwebNftMedia,
-  useContract,
-  useNFT,
-  Web3Button,
-} from "@thirdweb-dev/react";
-import type { FC } from "react";
-import {
-  nftDropContractAddress,
-  stakingContractAddress,
-} from "../consts/contractAddresses";
-import styles from "../styles/Home.module.css";
+import { client } from "@/app/client";
+import { NFT, prepareContractCall } from "thirdweb";
+import { MediaRenderer, TransactionButton } from "thirdweb/react";
+import { NFT_CONTRACT, STAKING_CONTRACT } from "../utils/contracts";
+import { useState } from "react";
+import { approve } from "thirdweb/extensions/erc721";
 
-interface NFTCardProps {
-  tokenId: number;
-}
-
-const NFTCard: FC<NFTCardProps> = ({ tokenId }) => {
-  const { contract } = useContract(nftDropContractAddress, "nft-drop");
-  const { data: nft } = useNFT(contract, tokenId);
-
-  return (
-    <>
-      {nft && (
-        <div className={styles.nftBox}>
-          {nft.metadata && (
-            <ThirdwebNftMedia
-              metadata={nft.metadata}
-              className={styles.nftMedia}
-            />
-          )}
-          <h3>{nft.metadata.name}</h3>
-          <Web3Button
-            action={(contract) => contract?.call("withdraw", [[nft.metadata.id]])}
-            contractAddress={stakingContractAddress}
-          >
-            Withdraw
-          </Web3Button>
-        </div>
-      )}
-    </>
-  );
+type OwnedNFTsProps = {
+    nft: NFT;
+    refetch: () => void;
+    refecthStakedInfo: () => void;
 };
-export default NFTCard;
+
+export const NFTCard = ({ nft, refetch, refecthStakedInfo }: OwnedNFTsProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isApproved, setIsApproved] = useState(false);
+
+    return (
+        <div style={{ margin: "10px" }}>
+            <MediaRenderer
+                client={client}
+                src={nft.metadata.image}
+                style={{
+                    borderRadius: "10px",
+                    marginBottom: "10px",
+                    height: "200px",
+                    width: "200px"
+                }}
+            />
+            <p style={{ margin: "0 10px 10px 10px"}}>{nft.metadata.name}</p>
+            <button
+                onClick={() => setIsModalOpen(true)}
+                style={{
+                    border: "none",
+                    backgroundColor: "#333",
+                    color: "#fff",
+                    padding: "10px",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                    width: "100%"
+                }}
+            >Stake</button>
+            {isModalOpen && (
+                <div style={{
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                    <div style={{
+                        minWidth: "300px",
+                        backgroundColor: "#222",
+                        padding: "20px",
+                        borderRadius: "10px",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center"
+                    }}>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                            width: "100%"
+                        }}>
+                            <button
+                                onClick={() => setIsModalOpen(false)}
+                                style={{
+                                    border: "none",
+                                    backgroundColor: "transparent",
+                                    color: "#fff",
+                                    cursor: "pointer"
+                                }}
+                            >Close</button>
+                        </div>
+                        <h3 style={{ margin: "10px 0" }}>You about to stake:</h3>
+                        <MediaRenderer
+                            client={client}
+                            src={nft.metadata.image}
+                            style={{
+                                borderRadius: "10px",
+                                marginBottom: "10px"
+                            }}
+                        />
+                        {!isApproved ? (
+                            <TransactionButton
+                                transaction={() => (
+                                    approve({
+                                        contract: NFT_CONTRACT,
+                                        to: STAKING_CONTRACT.address,
+                                        tokenId: nft.id
+                                    })
+                                )}
+                                style={{
+                                    width: "100%"
+                                }}
+                                onTransactionConfirmed={() => setIsApproved(true)}
+                            >Approve</TransactionButton>
+                        ) : (
+                            <TransactionButton
+                                transaction={() => (
+                                    prepareContractCall({
+                                        contract: STAKING_CONTRACT,
+                                        method: "stake",
+                                        params: [[nft.id]]
+                                    })
+                                )}
+                                onTransactionConfirmed={() => {
+                                    alert("Staked!");
+                                    setIsModalOpen(false);
+                                    refetch();
+                                    refecthStakedInfo();
+                                }}
+                                style={{
+                                    width: "100%"
+                                }}
+                            >Stake</TransactionButton>
+                        )}
+                        
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+};
